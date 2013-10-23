945f15c23177165d9de177212dc9c446a4013350
Dan Rose <dan.rose.uk@gmail.com>
2013-10-23 20:25:46 +0100

    X marks the spot! AKA draw dots to target and put an X on the destination

--- corsairs.xna/corsairs.xna/scenes/worldmap/WMShip.cs
+++ corsairs.xna/corsairs.xna/scenes/worldmap/WMShip.cs
@@ -13,10 +13,14 @@ namespace corsairs.xna.scenes.worldmap
     {
         protected Vector2 pos;
         protected Vector2 dest;
+        protected Vector2 velocity;
         protected Texture2D shipTexture;
         protected SpriteBatch spriteBatch;
         protected WorldMapScene worldMap;
         protected DateTime? leftClicked;
+        protected List<Vector2> dots = new List<Vector2>();
+        protected Texture2D lineTexture;
+        protected bool moving;
 
         protected static TimeSpan ClickThreshold = new TimeSpan(0, 0, 0, 0, 100);
 
@@ -32,9 +36,23 @@ namespace corsairs.xna.scenes.worldmap
             spriteBatch.Begin();
             base.Draw(gameTime);
 
-            spriteBatch.Draw(shipTexture, new Rectangle((int)pos.X, (int)pos.Y, 23, 29),
-               new Rectangle(0, 0, 23, 29),
-               Color.White);
+            if (moving)
+            {
+                spriteBatch.Draw(lineTexture, new Rectangle((int)dest.X - 11, (int)dest.Y - 10, 22, 19),
+                     new Rectangle(0, 0, 22, 19),
+                     Color.White);
+            }
+
+            foreach (var dot in dots)
+            {
+                spriteBatch.Draw(lineTexture, new Rectangle((int)dot.X, (int)dot.Y, 8, 8),
+                    new Rectangle(24, 24, 8, 8),
+                    Color.White);
+            }
+
+            spriteBatch.Draw(shipTexture, new Rectangle((int)pos.X - 11, (int)pos.Y - 14, 23, 29),
+             new Rectangle(0, 0, 23, 29),
+             Color.White);
 
             spriteBatch.End();
         }
@@ -72,6 +90,7 @@ namespace corsairs.xna.scenes.worldmap
             base.LoadContent();
 
             shipTexture = Game.Content.Load<Texture2D>("ship");
+            lineTexture = Game.Content.Load<Texture2D>("dots");
         }
 
         public override void Initialize()
@@ -92,6 +111,7 @@ namespace corsairs.xna.scenes.worldmap
                 {
                     dest = new Vector2(mouse.X, mouse.Y);
                     leftClicked = null;
+                    ResetDestination();
                 }
                 else if (leftClicked == null)
                 {
@@ -106,36 +126,50 @@ namespace corsairs.xna.scenes.worldmap
             }
         }
 
+        protected virtual void ResetDestination()
+        {
+            // work out new velocity vector
+            var direction = dest - pos; 
+            var normDirection = direction;
+            normDirection.Normalize();
+            float speed = 0.1f;
+            velocity = speed * normDirection;  
+
+            // clear old path
+            dots.Clear();
+
+            // draw new path
+            var step = direction.Multiply(1f / 10);
+            for (var i = 1; i < 10; i++)
+            {
+                dots.Add(pos + step.Multiply(i));
+            }
+        }
+
         protected virtual void MoveToDestination(GameTime gameTime)
         {
-            if (dest != Vector2.Zero && dest != pos)
+            var direction = dest - pos;
+
+            // finish move if within 5px
+            if (direction.ToLength() < 5)
             {
-                var direction = dest - pos;
+                pos = dest;
+                return;
+            }
 
-                // finish move if within 5px
-                if (direction.ToLength() < 5)
-                {
-                    pos = dest;
-                    return;
-                }
+            var distance = velocity.Multiply((float)gameTime.ElapsedGameTime.TotalMilliseconds);
+            var destination = pos + distance;
+            var destSquare = worldMap.Map.Locations[(int)destination.X / WorldMapScene.SquareSize, (int)destination.Y / WorldMapScene.SquareSize];
 
-                direction.Normalize();
-                float speed = 0.1f;
-
-                var distance = direction.Multiply((float)(speed * gameTime.ElapsedGameTime.TotalMilliseconds));
-                var destination = pos + distance;
-                var destSquare = worldMap.Map.Locations[(int)destination.X / WorldMapScene.SquareSize, (int)destination.Y / WorldMapScene.SquareSize];
-
-                if (destSquare.IsWater)
-                {
-                    pos += distance;
-                }
-                else
-                {
-                    // hit land - stop!
-                    dest = pos;
-                    return;
-                }
+            if (destSquare.IsWater)
+            {
+                pos += distance;
+            }
+            else
+            {
+                // hit land - stop!
+                dest = pos;
+                return;
             }
         }
 
@@ -146,8 +180,14 @@ namespace corsairs.xna.scenes.worldmap
             var keyboard = Keyboard.GetState();
             var mouse = Mouse.GetState();
 
+            moving = dest != Vector2.Zero && dest != pos;
+
             CheckForDestinationUpdate(mouse);
-            MoveToDestination(gameTime);   
+
+            if (moving)
+            {
+                MoveToDestination(gameTime);
+            }
         }
     }
 }
